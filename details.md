5eb0c295abfb687785a75a5ea3f55dacb211e6ea
Vania <vania.baek@gmail.com>
2016-06-25 23:07:46 -0700

    Initial commit

---
+++ details.md
@@ -0,0 +1,3 @@
+# vsts-open-work-items-in-excel #
+
+This extension places an option within Query context menu to open the Query result in Excel from the Web.
---
+++ gruntfile.js
@@ -0,0 +1,66 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        ts: {
+            build: {
+                tsconfig: true
+            },
+            options: {
+                fast: 'never'
+            }
+        },
+        exec: {
+            package_dev: {
+                command: "tfx extension create --root dist --manifest-globs vss-extension.json --overrides-file configs/dev.json",
+                stdout: true,
+                stderr: true
+            },
+            package_release: {
+                command: "tfx extension create --root dist --manifest-globs vss-extension.json --overrides-file configs/release.json",
+                stdout: true,
+                stderr: true
+            },
+            publish_dev: {
+                command: "tfx extension publish --service-url https://marketplace.visualstudio.com --root dist --manifest-globs vss-extension.json --overrides-file configs/dev.json",
+                stdout: true,
+                stderr: true
+            },
+            publish_release: {
+                command: "tfx extension publish --service-url https://marketplace.visualstudio.com --root dist --manifest-globs vss-extension.json --overrides-file configs/release.json",
+                stdout: true,
+                stderr: true
+            }
+        },
+        copy: {
+            scripts: {
+                files: [{
+                    expand: true, 
+                    flatten: true, 
+                    src: ["node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js"], 
+                    dest: "dist/scripts",
+                    filter: "isFile" 
+                },
+                {
+                    expand: true, 
+                    flatten: false, 
+                    src: ["styles/**", "img/**", "*.html", "vss-extension.json", "*.md"], 
+                    dest: "dist"
+                }]
+            }
+        },
+        
+        clean: ["scripts/**/*.js", "*.vsix", "dist"]
+    });
+    
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks("grunt-exec");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask("build", ["ts:build", "copy:scripts"]);
+    grunt.registerTask("package-dev", ["build", "exec:package_dev"]);
+    grunt.registerTask("package-release", ["build", "exec:package_release"]);
+    grunt.registerTask("publish-dev", ["package-dev", "exec:publish_dev"]);        
+    grunt.registerTask("publish-release", ["package-release", "exec:publish_release"]);        
+    
+    grunt.registerTask("default", ["package-dev"]);
+};
