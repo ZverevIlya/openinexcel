f0183865848e65628cf57376262807305b42ee6a
Vania <vania.baek@gmail.com>
2016-06-26 12:08:13 -0700

    first commit

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,20 +1,14 @@
-var actionProvider =  {
+var action =  {
     getMenuItems: (context) => {
         return [<IContributedMenuItem>{
-            title: "Work Item Menu Action",
+            title: "Open in Excel",
             action: (actionContext) => {
-                let workItemId = actionContext.id
-                    || (actionContext.ids && actionContext.ids.length > 0 && actionContext.ids[0])
-                    || (actionContext.workItemIds && actionContext.workItemIds.length > 0 && actionContext.workItemIds[0]);
-                    
-                if (workItemId) {
-                    alert(`Selected work item ${workItemId}`);
-                }
+                let queryId = actionContext.id
             }
         }];
     }
 };
 
 // Register context menu action provider
-VSS.register("<your-publisher>.vsts-extension-ts-seed-simple.vsts-extension-ts-seed-simple-action", actionProvider);
-VSS.register("vsts-extension-ts-seed-simple-action", actionProvider);
+VSS.register("vanvanvania.vsts-open-work-items-in-excel.vsts-open-work-items-in-excel-simple-action", action);
+VSS.register("vsts-open-work-items-in-excel-simple-action", action);
