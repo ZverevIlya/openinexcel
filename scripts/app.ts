345718fe070d185a224428eca721fca14cfb5f51
Vania Kurniawati (InConsulting Inc.) <v-vakurn@microsoft.com>
2016-07-05 16:08:13 -0700

    Added another action to select multiple work items instead of query, working commit

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,8 +1,10 @@
+
 var myAction =  {
     getMenuItems: (context) => {
         return [<IContributedMenuItem>{
             title: "Open in Excel",
             action: (actionContext) => { 
+
             	//From actionContext, get the query ID selected
                 var qid = actionContext["query"]["id"];
 
@@ -17,5 +19,28 @@ var myAction =  {
     }
 };
 
+var openWorkItemsAction =  {
+    getMenuItems: (context) => {
+        return [<IContributedMenuItem>{
+            title: "Open in Excel",
+            action: (actionContext) => { 
+
+            	//From actionContext, get the query ID selected
+                var qid = actionContext["query"]["id"];
+
+                //From web context, get collectionUri and projectName
+                var context = VSS.getWebContext();
+                var collectionUri = context["collection"]["uri"];
+                var projectName = context["project"]["name"];
+
+                window.location.href="tfs://ExcelRequirements/OpenItems?cn="+collectionUri+"&proj="+projectName+"&qid="+qid;
+            }
+        }];
+    }
+};
+
+
+
 // Register context menu action provider
 VSS.register("myAction", myAction);
+VSS.register("openWorkItemsAction", openWorkItemsAction);
