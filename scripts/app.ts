c0194de0d31f917fd9332d961150f8c1f122c704
Vania Kurniawati (InConsulting Inc.) <v-vakurn@microsoft.com>
2016-07-11 18:15:45 -0700

    Now will not show context menu for auto-generated queries such as Assigned to Me, Unsaved Work Items

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,6 +1,14 @@
-
 var openQueryAction =  {
     getMenuItems: (context) => {
+    	var qid = context["query"]["id"];
+    	var uQid = qid.toUpperCase();
+
+    	var staticQueryIds = ["A2108D31-086C-4FB0-AFDA-097E4CC46DF4", "B7A26A56-EA87-4C97-A504-3F028808BB9F", "202230E0-821E-401D-96D1-24A7202330D0", "53FB153F-C52C-42F1-90B6-CA17FC3561A8", "2CBF5136-1AE5-4948-B59A-36F526D9AC73", "08E20883-D56C-4461-88EB-CE77C0C7936D", "2650C586-0DE4-4156-BA0E-14BCFB664CCA"]
+
+    	if (staticQueryIds.indexOf(uQid) >= 0) {
+    		return null; 
+    	}
+    	else {
         return [<IContributedMenuItem>{
             title: "Requires DEV15",
         	text: "Open in Excel",
@@ -24,6 +32,7 @@ var openQueryAction =  {
                 }
             }
         }];
+    }
     }
 };
 
