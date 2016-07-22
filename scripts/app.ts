2b7a0b0e2804311311287a3391a63acc56c399ac
Vania Kurniawati (InConsulting Inc.) <v-vakurn@microsoft.com>
2016-07-22 15:55:15 -0700

    working commit for openinexcel new toolbar extension point

--- scripts/app.ts
+++ scripts/app.ts
@@ -5,7 +5,7 @@ var openQueryAction =  {
 
     	var staticQueryIds = { "A2108D31-086C-4FB0-AFDA-097E4CC46DF4": true, "B7A26A56-EA87-4C97-A504-3F028808BB9F": true, "202230E0-821E-401D-96D1-24A7202330D0": true, "53FB153F-C52C-42F1-90B6-CA17FC3561A8": true, "2CBF5136-1AE5-4948-B59A-36F526D9AC73": true, "08E20883-D56C-4461-88EB-CE77C0C7936D": true, "2650C586-0DE4-4156-BA0E-14BCFB664CCA": true};
 
-    	if (staticQueryIds[uQid]) {
+    	if (staticQueryIds[uQid] == true) {
     		return null; 
     	}
     	else {
@@ -17,19 +17,14 @@ var openQueryAction =  {
             	//From actionContext, get the query ID selected
                 var qid = actionContext["query"]["id"];
 
-                if (qid == "a2108d31-086c-4fb0-afda-097e4cc46df4") {
-                	alert("Only custom queries can be exported to Excel. To export this query, save this as a new query.");
-                }
+                //From web context, get collectionUri and projectName
+                var context = VSS.getWebContext();
+                var collectionUri = context["collection"]["uri"];
+                var projectName = context["project"]["name"];
 
-                else {
-	                //From web context, get collectionUri and projectName
-	                var context = VSS.getWebContext();
-	                var collectionUri = context["collection"]["uri"];
-	                var projectName = context["project"]["name"];
-
-	                var uri = "tfs://ExcelRequirements/OpenQuery?cn="+collectionUri+"&proj="+projectName+"&qid="+qid;
-	                window.location.href= uri;
-                }
+                var uri = "tfs://ExcelRequirements/OpenQuery?cn="+collectionUri+"&proj="+projectName+"&qid="+qid;
+                window.location.href= uri;
+                
             }
         }];
     }
@@ -44,25 +39,39 @@ var openWorkItemsAction =  {
 				var wids = actionContext.ids;
                 var columns = actionContext["columns"];
 
+                alert(JSON.stringify(actionContext));
+
                 //From web context, get collectionUri and projectName
-                var context = VSS.getWebContext();
+                /*var context = VSS.getWebContext();
                 var collectionUri = context["collection"]["uri"];
                 var projectName = context["project"]["name"];
 
-                var uri = "tfs://ExcelRequirements/OpenSelections?cn="+collectionUri+"&proj="+projectName+"&wids="+wids+"&columns="+columns;
+                var uri = "tfs://ExcelRequirements/OpenItems?cn="+collectionUri+"&proj="+projectName+"&wids="+wids+"&columns="+columns;
                 var n = uri.length;
                 if (n > 2000) {
                 	alert("You've selected too many fields and/or Work Items. Try exporting the Work Item Query instead.")
                 } 
                 else {
-                	window.location.href= uri;
+                    window.location.href= uri;
                 }
+                */
             }
         }];
     }
 };
 
+var openQueryOnToolbarAction = {
+	getMenuItems: (context) => {
+		return [<IContributedMenuItem>{
+			title: "Open in Excel",
+			action: (actionContext) => {
+				alert(JSON.stringify(actionContext));
+			}
+		}];
+	}
+}
 
 // Register context menu action provider
 VSS.register("openQueryAction", openQueryAction);
 VSS.register("openWorkItemsAction", openWorkItemsAction);
+VSS.register("openQueryOnToolbarAction", openQueryOnToolbarAction);
