bb445bd4dd5e96126688a8bc9ed4019231b8e9c7
Vania Kurniawati (InConsulting Inc.) <v-vakurn@microsoft.com>
2016-08-03 20:25:42 -0700

    second test

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,4 +1,15 @@
-import { TestHelper } from "./testHelper";
+function checkForStaticQuery(qid: string) {
+    var staticQueryIds = { "A2108D31-086C-4FB0-AFDA-097E4CC46DF4": true, "B7A26A56-EA87-4C97-A504-3F028808BB9F": true, "202230E0-821E-401D-96D1-24A7202330D0": true, "53FB153F-C52C-42F1-90B6-CA17FC3561A8": true, "2CBF5136-1AE5-4948-B59A-36F526D9AC73": true, "08E20883-D56C-4461-88EB-CE77C0C7936D": true, "2650C586-0DE4-4156-BA0E-14BCFB664CCA": true};
+
+    var uQid = qid.toUpperCase();
+
+    if (staticQueryIds[uQid]) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 var openQueryAction =  {
     getMenuItems: (context) => {
@@ -18,10 +29,6 @@ var openQueryAction =  {
             action: (actionContext) => { 
                 
                 var qid = actionContext["query"]["id"];
-                if (qid) {
-                    let testHelper = new TestHelper();
-                    alert(testHelper.format(qid));
-                }
 
                 var context = VSS.getWebContext();
                 var collectionUri = context["collection"]["uri"];
--- scripts/testHelper.tests.ts
+++ scripts/testHelper.tests.ts
@@ -1,13 +1,22 @@
 /// <reference path="../typings/index.d.ts" />
 
-import { expect } from 'chai';
-
+import { expect } from "chai";
+import { app } from "./app";
 import { TestHelper } from "./testHelper";
 
-describe("Guid check", () => {
-    it("should return query ID", () => {
-        let testHelper = new TestHelper();
-        
-        expect(testHelper.format("67f50c68-d311-4f45-8bd0-da7268f02435")).to.be.equal("67f50c68-d311-4f45-8bd0-da7268f02435");
+describe("Checks for valid input processing", () => {
+    describe("Guid check", () => {
+        it("should return query ID", () => {
+            let testHelper = new TestHelper();
+            expect(testHelper.format("67f50c68-d311-4f45-8bd0-da7268f02435")).to.be.equal("67f50c68-d311-4f45-8bd0-da7268f02435");
+        });
     });
-});
+
+    describe("Guid check for static queries", () => {
+        it("should return true for known static query guids", () => {
+            let app = new app();
+            expect(app.checkForStaticQuery("2650C586-0DE4-4156-BA0E-14BCfb664cca")).to.be.equal(true);
+        });
+    });
+}); 
+
