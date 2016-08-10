02945b7e8a1eb96a742841b5fe70e68560b8c1ab
Vania Kurniawati (InConsulting Inc.) <v-vakurn@microsoft.com>
2016-08-10 14:20:46 -0700

    added more unit tests

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,4 +1,11 @@
-export var staticQueryIds = { "A2108D31-086C-4FB0-AFDA-097E4CC46DF4": true, "B7A26A56-EA87-4C97-A504-3F028808BB9F": true, "202230E0-821E-401D-96D1-24A7202330D0": true, "53FB153F-C52C-42F1-90B6-CA17FC3561A8": true, "2CBF5136-1AE5-4948-B59A-36F526D9AC73": true, "08E20883-D56C-4461-88EB-CE77C0C7936D": true, "2650C586-0DE4-4156-BA0E-14BCFB664CCA": true};
+export var staticQueryIds = { 
+    "A2108D31-086C-4FB0-AFDA-097E4CC46DF4": "ASSIGNED_TO_ME_ID", 
+    "B7A26A56-EA87-4C97-A504-3F028808BB9F": "UNSAVED_WORKITEMS_ID", 
+    "202230E0-821E-401D-96D1-24A7202330D0": "FOLLOWED_WORKITEMS_ID", 
+    "53FB153F-C52C-42F1-90B6-CA17FC3561A8": "CREATED_BY_ME_ID", 
+    "2CBF5136-1AE5-4948-B59A-36F526D9AC73": "SEARCH_RESULTS_ID", 
+    "08E20883-D56C-4461-88EB-CE77C0C7936D": "CUSTOM_WIQL_QUERY_ID", 
+    "2650C586-0DE4-4156-BA0E-14BCFB664CCA": "RECYCLE_BIN_QUERY_ID"};
 
 export function checkForStaticQuery(qid: string) {
     if (staticQueryIds[qid.toUpperCase()]) {
@@ -13,10 +20,23 @@ export function createUri(action: string, cn: string, proj: string, qid?: string
     var uri = "tfs://ExcelRequirements/"+action+"?cn="+cn+"&proj="+proj;
 
     if (action == "OpenItems") {
+        if (wids) {
         uri = uri+"&wid="+wids+"&columns="+columns;
+        }
+        else {
+            return null;
+        }
     } 
+    else if (action == "OpenQuery"){
+        if (qid) {
+            uri = uri+"&qid="+qid;
+        }
+        else {
+            return null;
+        }
+    }
     else {
-        uri = uri+"&qid="+qid;
+        return null;
     }
 
     if (uri.length > 2000) {
--- scripts/testHelper.tests.ts
+++ scripts/testHelper.tests.ts
@@ -1,19 +1,42 @@
 /// <reference path="../typings/index.d.ts" />
 
 import { expect } from "chai";
-import { checkForStaticQuery } from "./app";
+import * as App from "./app";
 
 describe("Checks for valid input processing", () => {
     describe("Guid check for static queries (false)", () => {
         it("should return false for not a known static query guid", () => {
-            expect(checkForStaticQuery("meow-mix-meow-mix")).to.be.equal(false);
+            expect(App.checkForStaticQuery("meow-mix-meow-mix")).to.be.equal(false);
         });
     });
 
     describe("Guid check for static queries", () => {
         it("should return true for known static query guids", () => {
-            expect(checkForStaticQuery("2650C586-0DE4-4156-BA0E-14BCfb664cca")).to.be.equal(true);
+            expect(App.checkForStaticQuery("2650C586-0DE4-4156-BA0E-14BCfb664cca")).to.be.equal(true);
+        });
+    });
+
+    describe("Return null if trying to open query without qid", () => {
+        it("should return null", () => {
+            expect(App.createUri("OpenQuery", "http://foo.bar", "fooBarProject", null, null, null)).to.be.null;
+        });
+    });
+
+    describe("Return a valid uri if opening query with valid qid", () => {
+        it("should return uri.length > 0", () => {
+            expect(App.createUri("OpenQuery", "http://foo.bar", "fooBarProject", "fooQueryId", null, null).length).to.be.above(0);
+        });
+    });
+
+    describe("Return null if trying to open items without wid", () => {
+        it("should return null", () => {
+            expect(App.createUri("OpenItems", "http://foo.bar", "fooBarProject", null, null, null)).to.be.null;
+        });
+    });
+
+    describe("Return a valid uri if opening items with valid wid", () => {
+        it("should return uri.length > 0", () => {
+            expect(App.createUri("OpenQuery", "http://foo.bar", "fooBarProject", "fooQueryId", "1,2,3", null).length).to.be.above(0);
         });
     });
 }); 
-
